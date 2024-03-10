bf47fa0fbeaeaac0a3b0cb165aaf3309d7f3fcd8
Mustafa-Moghazy <73305128+Mustafa-Moghazy@users.noreply.github.com>
2024-03-10 17:59:39 +0200

    add coupon creation form component

--- Frontend/final-project/src/app/app.routes.ts
+++ Frontend/final-project/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CouponFormComponent } from './components/coupon-form/coupon-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'create-coupon', component: CouponFormComponent },
+];
