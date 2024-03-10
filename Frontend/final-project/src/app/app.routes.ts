b8b0a15f18a9d8cc95ccf1c984c26cc86373a67e
Mustafa-Moghazy <73305128+Mustafa-Moghazy@users.noreply.github.com>
2024-03-10 23:33:35 +0200

    add home component

--- Frontend/final-project/src/app/app.routes.ts
+++ Frontend/final-project/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CouponFormComponent } from './components/coupon-form/coupon-form.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'create-coupon', component: CouponFormComponent },
 ];
