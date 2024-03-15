b7f35e5ede925d8039cc77dd6fbdf976504f7e2a
Mustafa-Moghazy <73305128+Mustafa-Moghazy@users.noreply.github.com>
2024-03-15 23:16:29 +0200

    add CouponRepository and setup service layer

---
+++ Backend/coupon/coupon/src/main/java/com/example/coupon/repository/CouponRepository.java
@@ -0,0 +1,9 @@
+package com.example.coupon.repository;
+
+import com.example.coupon.entity.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+}
