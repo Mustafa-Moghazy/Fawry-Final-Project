1ea049aee71035a9990b4063e12426ebaca814fa
Mustafa-Moghazy <73305128+Mustafa-Moghazy@users.noreply.github.com>
2024-03-15 22:49:51 +0200

    setup project files and dependencies

---
+++ Backend/coupon/coupon/src/main/java/com/example/coupon/CouponApplication.java
@@ -0,0 +1,13 @@
+package com.example.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponApplication.class, args);
+	}
+
+}
